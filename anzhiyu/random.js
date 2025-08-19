741fdbc1187fcaea074e6579d16ab864709f7f14
kuruplanet <justing0601@163.com>
2025-08-19 11:15:51 +0800

    Site updated: 2025-08-19 11:15:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/19/LuckyGameTools下载/","2025/08/19/hello-world/","2025/08/19/推荐typora/","2025/08/18/经典版Flash Player下载/"];function toRandomPost(){
+var posts=["2025/08/19/LuckyGameTools下载/","2025/08/19/推荐typora/","2025/08/19/hello-world/","2025/08/18/经典版Flash Player下载/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
